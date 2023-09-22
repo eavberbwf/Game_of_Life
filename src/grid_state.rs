3a34adfdf1a2ea8a6043965f7f6155daf510e1cb
eavberbwf <emilioavalle@berkeley.edu>
2023-09-21 23:09:19 -0700

    Discretized into States and Transitions
    Created enum types to generically represent alive and dead, and all the transition states
    implemented displays and relevant methods for each Grid<T>

---
+++ src/grid_state.rs
@@ -0,0 +1,92 @@
+use crate::grid_implementation::Grid;
+use crate::grid_implementation::States;
+use crate::grid_implementation::States::Alive;
+use crate::grid_implementation::States::Dead;
+use crate::grid_implementation::SIZE;
+
+use std::fmt;
+
+impl Grid<States>{
+    
+    fn toroidal(&self, (x, y): (i8, i8)) -> States {
+        let sz = SIZE as i8;
+        let (xmod, ymod) = (x.rem_euclid(sz) as usize, y.rem_euclid(sz) as usize);
+
+        self[(xmod, ymod)]
+    }
+
+    fn rand_array(_: usize) -> [States; SIZE] {
+        rand::random()
+    }
+
+    fn rand_matrix() -> [[States; SIZE]; SIZE] {
+        core::array::from_fn(Self::rand_array)
+    }
+
+    pub fn new() -> Grid<States> {
+        Grid {
+            matrix: Self::rand_matrix(),
+        }
+    }
+
+    fn get_neighbor_count(&self, (x, y): (i8, i8)) -> u8 {
+        let nghbhd: [(i8, i8); 9] = [
+            (x - 1, y - 1),
+            (x - 1, y),
+            (x - 1, y + 1),
+            (x, y - 1),
+            (x, y + 1),
+            (x + 1, y - 1),
+            (x + 1, y),
+            (x + 1, y + 1),
+            (x, y),
+        ];
+
+        nghbhd
+            .into_iter()
+            .fold(0, |acc, b| acc + (self.toroidal(b) as u8))
+    }
+
+    pub fn next(&self) -> Grid<States> {
+        let mut next_gen = self.clone();
+
+        for i in 0..SIZE {
+            for j in 0..SIZE {
+                let nghbr_count: u8 = next_gen.get_neighbor_count((i as i8, j as i8));
+
+                match nghbr_count {
+                    3 => next_gen.matrix[i][j] = Alive,
+                    4 => continue,
+                    _ => next_gen.matrix[i][j] = Dead,
+                }
+            }
+        }
+        next_gen
+    }
+
+    fn state_to_emote(state: States) -> String {
+        match state {
+            Alive => "🌕".to_string(),
+            Dead => "🌑".to_string(),
+        }
+    }
+
+    fn array_to_emotes(line: [States; SIZE]) -> String {
+        line.into_iter().fold(String::new(), |acc, state| {
+            acc + &Self::state_to_emote(state)
+        })
+    }
+}
+
+impl fmt::Display for Grid<States> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.matrix.into_iter().fold(String::new(), |acc, line| {
+                acc + &Grid::array_to_emotes(line) + "\n"
+            })
+        )
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
+mod grid_state;
+mod grid_transitions;
+
 mod grid_implementation;
 use crate::grid_implementation::Grid;
+use crate::grid_implementation::States;
+use crate::grid_implementation::Transitions;
+use crate::grid_implementation::Transitions::A1;
+use crate::grid_implementation::Transitions::A3;
+
 
 fn main() {
-    let griddy = Grid::new();
+    let griddy = Grid::<States>::new();
 
-    println!("{}", griddy[(0, 0)]);
+    let trans: Grid<Transitions> = Grid { matrix: [
+        [A3, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+        [A1, A1, A1, A1, A1, A1, A1, A1, A1, A1],
+    ]};
+    println!("{}", trans);
+
     println!("{}", griddy);
     println!("{}", griddy.next());
     println!("{}", griddy.len());
