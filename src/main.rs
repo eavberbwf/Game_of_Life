8adcfba58207de8c1c226a64a3fa2afad741422a
eavberbwf <emilioavalle@berkeley.edu>
2023-09-23 16:42:34 -0700

    added ignore

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,6 @@ mod sequence_gen;
 
 fn main() {
     let g: Grid<States> = Grid::new();
-    println!("{}", g);
-    println!("{}", g.next());
-
-    use ndarray::{array, Zip};
-
-    let k = sequence_gen::grids_to_sequence_grid(&g, &g.next());
-
-    println!("{}", k[[0, 0, 0]]);
-    println!("{}", k[[1, 0, 0]]);
-    println!("{}", k[[2, 0, 0]]);
-    println!("{}", k[[3, 0, 0]]);
-    println!("{}", k[[4, 0, 0]]);
-
     for grid in sequence_gen::seq_to_grids(&g, &g.next()) {
         println!("{}", grid);
     }
